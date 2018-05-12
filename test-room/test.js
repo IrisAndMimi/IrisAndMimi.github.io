d2a5af8d1a328ed878609c7654c7b1c21b8807eb
IrisAndMimi <my1ris227@outlook.com>
2018-05-12 09:25:49 +0800

    是否有按鈕

--- test-room/test.js
+++ test-room/test.js
@@ -1,1 +1,9 @@
-document.write("ya!! js here!!");
+var cars = ["Saab", "Volvo", "BMW","Mimi"];
+cars[cars.length] =("Oops");
+document.write(cars);
+var a = cars.length;
+function myFunction() {
+	var x = Math.floor((Math.random() * (a-1)) );
+}
+document.write(a);
+document.getElementById("demo").innerHTML = cars[x];
