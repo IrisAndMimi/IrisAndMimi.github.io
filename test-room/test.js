72bac8c382f412ce77cdc4076969236f97af1783
IrisAndMimi <my1ris227@outlook.com>
2018-05-12 09:48:35 +0800

    function向下移動

--- test-room/test.js
+++ test-room/test.js
@@ -2,8 +2,9 @@ var cars = ["Saab", "Volvo", "BMW","Mimi"];
 cars[cars.length] =("Oops");
 document.write(cars);
 var a = cars.length;
+document.write(a);
 function myFunction() {
 	var x = Math.floor((Math.random() * (a-1)) );
+	document.getElementById("demo").innerHTML = cars[x];
 }
-document.write(a);
-document.getElementById("demo").innerHTML = cars[x];
+
