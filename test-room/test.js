4b9c7560d5088f102f49cb3670f088791fce80e1
IrisAndMimi <my1ris227@outlook.com>
2018-05-12 16:38:31 +0800

    可讀否?

--- test-room/test.js
+++ test-room/test.js
@@ -7,8 +7,6 @@ function myFunction() {
 	var x = Math.floor((Math.random() * a) );
 	document.getElementById("rdm-btn").innerHTML = cars[x];
 }
-
-document.getElementById("edit_area").contentEditable = true ;
 function output() {
   var sourceArea = document.getElementById("source_code");
   var editArea = document.getElementById("edit_area");
