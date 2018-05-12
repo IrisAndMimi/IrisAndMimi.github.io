b9ad057b753afe24e65fb92ac4ced53f66684ce7
IrisAndMimi <my1ris227@outlook.com>
2018-05-12 16:27:34 +0800

    可以輸入?

--- test-room/test.js
+++ test-room/test.js
@@ -5,6 +5,12 @@ var a = cars.length;
 document.write(a);
 function myFunction() {
 	var x = Math.floor((Math.random() * a) );
-	document.getElementById("demo").innerHTML = cars[x];
+	document.getElementById("rdm-btn").innerHTML = cars[x];
 }
 
+document.getElementById("edit_area").contentEditable = true ;
+function output() {
+  var sourceArea = document.getElementById("source_code");
+  var editArea = document.getElementById("edit_area");
+  sourceArea.innerHTML= editArea.innerHTML;
+}
