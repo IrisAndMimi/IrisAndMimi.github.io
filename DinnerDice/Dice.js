5c7fed315b203188dbc7f7e281628087ab14b5d0
IrisAndMimi <my1ris227@outlook.com>
2018-05-15 23:49:08 +0800

    明天大概是個可憎的抓巴時光

---
+++ DinnerDice/Dice.js
@@ -0,0 +1,18 @@
+var fds = ["Saab", "Volvo", "BMW","Mimi"];
+var a = fds.length;
+$('#btn-start').addEventListener('click', function(){
+	function myFunction() {
+	var x = Math.floor((Math.random() * a) );
+	document.getElementById("Dice-rdm").innerHTML = fds[x];
+	}
+
+});
+
+
+function output() {
+	var sourceArea = document.getElementById("source_code");
+	var editArea = document.getElementById("edit_area");
+	sourceArea.innerHTML= editArea.innerHTML;
+	fds[fds.length] =editArea.innerHTML;
+	document.getElementById("now-string").innerHTML = fds;
+}
