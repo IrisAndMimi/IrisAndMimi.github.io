46a56201b8120dca66d5029f8da610bdc12c910c
IrisAndMimi <my1ris227@outlook.com>
2018-05-16 17:15:55 +0800

    十之八九沒功用

--- DinnerDice/Dice.js
+++ DinnerDice/Dice.js
@@ -1,10 +1,13 @@
 var fds = ["Saab", "Volvo", "BMW","Mimi"];
 var a = fds.length;
-function myFunction() {
+function diceClick() {
 	var x = Math.floor((Math.random() * a) );
 	document.getElementById("Dice-rdm").innerHTML = fds[x];
 	}
-
+function chgBkgd() {
+	var x = Math.floor((Math.random() * a) );
+	document.getElementById("Dice-rdm").innerHTML = fds[x];
+	}
 function output() {
 	var sourceArea = document.getElementById("source_code");
 	var editArea = document.getElementById("edit_area");
@@ -12,3 +15,10 @@ function output() {
 	fds[fds.length] =editArea.innerHTML;
 	document.getElementById("now-string").innerHTML = fds;
 }
+$(document).ready(function()
+{
+    $('#bkgd-lavender, #bkgd-dirtaqua').hide(); 
+    second = new Date().getSeconds();
+    if ((second % 2) == 0) $('#bkgd-lavender').show();
+    if ((second % 2) == 1) $('#bkgd-dirtaqua').show();
+});
