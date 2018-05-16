8d70ee1047fdbc637c3632b5568a38b7fd160ae5
IrisAndMimi <my1ris227@outlook.com>
2018-05-16 23:03:06 +0800

    我覺得還是可以偶爾丁丁一下

--- DinnerDice/Dice.js
+++ DinnerDice/Dice.js
@@ -2,7 +2,7 @@ var fds = ["Saab", "Volvo", "BMW","Mimi"];
 var a = fds.length;
 var bg=document.getElementById('container').value;
 function diceClick() {
-	var x = Math.floor((Math.random() * a) );
+	var x = Math.floor( (Math.random() * a) );
 	document.getElementById("Dice-rdm").innerHTML = fds[x];
 }
 function chgBgLvdr() {
@@ -25,10 +25,13 @@ function output() {
 	fds[fds.length] =editArea.innerHTML;
 	document.getElementById("now-string").innerHTML = fds;
 }
-$(document).ready(function()
+$(document).ready( function()
 {
-    $('#bkgd-lavender, #bkgd-dirtaqua').hide(); 
-    second = new Date().getSeconds();
-    if ((second % 2) == 0) $('#bkgd-lavender').show();
-    if ((second % 2) == 1) $('#bkgd-dirtaqua').show();
+    var second = new Date().getSeconds();
+    if ( (second % 2) == 0){
+		document.body.style.backgroundColor= "#B57EDC";
+	}else if ( (second % 2) == 1){
+		document.body.style.backgroundColor= "#00E0E0";
+	}
+    
 });
