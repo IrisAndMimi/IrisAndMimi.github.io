b052d9b88b42a30c6e8118e5716bb2f86234cb34
IrisAndMimi <my1ris227@outlook.com>
2018-05-17 22:08:49 +0800

    雖然怪還是試試吧

--- DinnerDice/Dice.js
+++ DinnerDice/Dice.js
@@ -1,15 +1,18 @@
-var fds = ["漢堡", "義大利麵", "披薩","牛排"];
+var usfds = ["漢堡", "義大利麵", "披薩","牛排"];
+var chfds = ["火鍋", "陽春麵", "便當","三明治","自助餐","炸物"];
 var a = fds.length;
-function diceClick() {
-	var x = Math.floor( (Math.random() * a) );
-	document.getElementById("Dice-rdm").innerHTML = fds[x];
-}
 function fdChsn() {
 	var us = document.getElementById("Us");
 	var ch=document.getElementById('Ch');
 	if(us.checked) {
-		document.getElementById("Dice-rdm").innerHTML = a;
+		var fds = usfds;\
+	}else if (ch.checked){
+		var fds = chfds;
 	}
+}
+function diceClick() {
+	var x = Math.floor( (Math.random() * a) );
+	document.getElementById("Dice-rdm").innerHTML = fds[x];
 }
 
 function chgBgLvdr() {
