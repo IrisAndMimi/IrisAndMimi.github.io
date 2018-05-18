d35b5a3c3a90f4b24029c6c36dd68b99fc797b8a
IrisAndMimi <32083426+IrisAndMimi@users.noreply.github.com>
2018-05-18 11:16:38 +0800

    唉

--- DinnerDice/Dice.js
+++ DinnerDice/Dice.js
@@ -12,7 +12,7 @@ function diceClick() {
 	var x = Math.floor( (Math.random() * a) );
 	document.getElementById("Dice-rdm").innerHTML = fds[x];
 	
-	if(loc == ?fd-sty=Us) {
+	if(loc == '?fd-sty=Us') {
 		document.write(loc);
 		alert('?fd-sty=Us');
 	}
