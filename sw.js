1f31554d8bb126a9f3c53f73b2a31961180c7816
IrisAndMimi <my1ris227@outlook.com>
2018-05-20 14:59:15 +0800

    開始分割

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-<script>
+
   if (navigator.serviceWorker != null) {
     navigator.serviceWorker.register('sw.js')
     .then(function(registration) {
       console.log('Registered events at scope: ', registration.scope);
     });
   }
-</script>
