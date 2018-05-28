276a94752597c760e5c7d20123aa9664c5377c15
IrisAndMimi <my1ris227@outlook.com>
2018-05-28 12:03:54 +0800

    陀螺儀0u0??

--- test-room/test.js
+++ test-room/test.js
@@ -13,3 +13,20 @@ function output() {
 	cars[cars.length] =editArea.innerHTML;
 	document.getElementById("now-string").innerHTML = cars;
 }
+if(window.DeviceOrientationEvent) {
+  window.addEventListener('deviceorientation', function(event) {
+    var a = document.getElementById('alpha'),
+        b = document.getElementById('beta'),
+        g = document.getElementById('gamma'),
+        alpha = event.alpha,
+        beta = event.beta,
+        gamma = event.gamma;
+
+    a.innerHTML = Math.round(alpha);
+    b.innerHTML = Math.round(beta);
+    g.innerHTML = Math.round(gamma);
+
+  }, false);
+}else{
+  document.querySelector('body').innerHTML = '你的瀏覽器不支援喔';
+}
