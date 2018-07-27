beb2df8ac6d3ce1f9c44053c16fce44975044d68
IrisAndMimi <32083426+IrisAndMimi@users.noreply.github.com>
2018-07-27 22:41:12 +0800

    會整個空掉嗎

--- test-room/test.js
+++ test-room/test.js
@@ -28,5 +28,5 @@ if(window.DeviceOrientationEvent) {
 
   }, false);
 }else{
-  document.querySelector('#alpha').innerHTML = '你的瀏覽器不支援呦OAQ';
+  document.querySelector('#gyro').innerHTML = '你的瀏覽器不支援呦OAQ';
 }
